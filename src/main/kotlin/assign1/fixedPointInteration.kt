288c57c35e22d6027365015fea40caaf4142a5f4
Isak-Evaldsson <isak.evaldsson@outlook.com>
2021-01-22 16:35:13 +0100

    Assignment 1 (task 4). Validation and minor fixes to the FPI algorithm.

--- src/main/kotlin/assign1/fixedPointInteration.kt
+++ src/main/kotlin/assign1/fixedPointInteration.kt
@@ -1,28 +1,28 @@
 package assign1
 
-import java.lang.Math.PI
-import java.lang.Math.pow
 import kotlin.math.*
 
 
-fun FPI(g: (Double) -> Double, f: (Double) -> Double, x0: Double): Double {
+fun FPI(g: (Double) -> Double, g_prime: (Double) -> Double,  r: Double, x0: Double): Double {
+    if(!valid(g_prime, x0, r)) {
+        throw Error("Invalid starting position x0")
+    }
     var x = x0
-    while (abs(f(x)) > 0.5e-8) {
+    while (abs(r - x) > 1e-9) {
+        println(abs(r - x))
         x = g(x)
     }
     return x
 }
 
-fun validate() {
-
-}
+fun valid(g_prime: (Double) -> Double, x0: Double, r: Double) : Boolean
+    = abs(g_prime(x0)) < 1 && abs(r - x0) <= 1
 
 fun main() {
+    val root = 1.12998049865083
     val g: (Double) -> Double = {x -> ln(4 - sin(x))} // Rewrittes base equation on the form g(x) = x
-    val f: (Double) -> Double = {x -> exp(x) + sin(x) - 4}
+    val g_prime: (Double) -> Double = {x -> -cos(x)/(4- sin(x))} // Derivative of g(x)
+    val x0 = 1.0 // Closest integer to solution
 
-    val g1: (Double) -> Double = {x -> cos(x)}
-    val f1: (Double) -> Double = {x -> cos(x) - x}
-    val x0 = 1.0 // The integer that is closest to the solution
-    print(FPI(g1, f1, x0))
+    print(FPI(g, g_prime, root, x0))
 }
